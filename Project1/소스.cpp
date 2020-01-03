f9885ab567d2f0bb7da75bdbe69f59245f649734
albert507 <albert507@naver.com>
2020-01-03 15:15:43 +0900

    Day1

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
 int main() {
+    int i;
+    int j=0;
+    int sum = 0;
     std::cout << "Hello, World!!!" << std::endl;
+
+
+    for (i = 0; i < 5; i++) {
+        std::cout << i << std::endl;
+        sum += i;
+    }
+
+    std::cout << sum << std::endl;
+    
+    
+    while (j < 5) {
+        std::cout << "hi" << std::endl;
+        j++;
+    }
+    
+    std::cout << "hi" << std::endl
+        << "my name is "
+        << "Psi" << std::endl;
+    
+    
+    
     return 0;
 }
