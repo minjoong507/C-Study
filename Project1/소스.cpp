58c928c25ccc6988295b3cbde031ccf559628378
albert507 <albert507@naver.com>
2020-01-03 15:33:38 +0900

    입출력 예제

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -25,6 +25,29 @@ int main() {
         << "Psi" << std::endl;
     
     
+    int lucky_num = 3;
+    int user_input;
+    std::cout << "숫자를 입력해보세용:" << std::endl;
     
+    while (1) {
+        std::cout << "입력" << std::endl;
+        std::cin >> user_input;
+        if (lucky_num == user_input) {
+            std::cout << "맞았습니다!" << std::endl;
+            break;
+        }
+        else {
+            std::cout << "다시 입력해보세요~" << std::endl;
+        }
+    }
+
+  
+
+
+
+
+
+
+
     return 0;
 }
