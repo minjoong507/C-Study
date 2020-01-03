bc4c23e228b5b14d36c9c03294cda13999e53c84
albert507 <albert507@naver.com>
2020-01-03 16:04:26 +0900

    함수 예제

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,53 +1,20 @@
 #include <iostream>
 
+
+void square_f(int arg);
+
 int main() {
+    
     int i;
-    int j=0;
-    int sum = 0;
-    std::cout << "Hello, World!!!" << std::endl;
-
-
-    for (i = 0; i < 5; i++) {
-        std::cout << i << std::endl;
-        sum += i;
-    }
-
-    std::cout << sum << std::endl;
-    
-    
-    while (j < 5) {
-        std::cout << "hi" << std::endl;
-        j++;
-    }
-    
-    std::cout << "hi" << std::endl
-        << "my name is "
-        << "Psi" << std::endl;
-    
-    
-    int lucky_num = 3;
     int user_input;
-    std::cout << "숫자를 입력해보세용:" << std::endl;
-    
-    while (1) {
-        std::cout << "입력" << std::endl;
-        std::cin >> user_input;
-        if (lucky_num == user_input) {
-            std::cout << "맞았습니다!" << std::endl;
-            break;
-        }
-        else {
-            std::cout << "다시 입력해보세요~" << std::endl;
-        }
-    }
+    std::cout << "제곱할 수?" << std::endl;
+    std::cin >> user_input;
 
-  
-
-
-
-
-
-
+    square_f(user_input);
 
     return 0;
+}
+
+void square_f(int arg) {
+    std::cout << arg * arg << std::endl;
 }
