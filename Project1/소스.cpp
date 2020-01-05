9b390cceef5357dcf7b7c6328b4babddd7f8a028
albert507 <albert507@naver.com>
2020-01-05 18:57:35 +0900

    new 배열 할당

--- Project1/소스.cpp
+++ Project1/소스.cpp
@@ -1,19 +1,17 @@
+
 #include <iostream>
 
-
-void change_num(int *arg);
-
 int main() {
-    
-    int i=5;
-    std::cout << "원래 숫자 :" << i << std::endl;
-    change_num(&i);
-    std::cout << "바뀐 숫자"
-
+    int arr_size;
+    std::cout << "array size : ";
+    std::cin >> arr_size;
+    int* list = new int[arr_size];
+    for (int i = 0; i < arr_size; i++) {
+        std::cin >> list[i];
+    }
+    for (int i = 0; i < arr_size; i++) {
+        std::cout << i << "th element of list : " << list[i] << std::endl;
+    }
+    delete[] list;
     return 0;
-}
-
-void change_num(int *arg) {
-    *arg = 3;
-
 }
